245abc7ed9f1e59b41caf54758ac32d6068f8848
Rachel <rbennett1984@gmail.com>
2019-03-06 19:26:18 -0500

    first commit

---
+++ model.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+//blog schema
+const blogPostSchema = mongoose.Schema({
+    title: {String, required: true},
+    author: [ {
+        firstName: String,
+        lastName: String
+    }],
+    content: {type: String},
+  created: {type: Date, default: Date.now}
+});
+
+//virtual property and serialize instance method 
+
+blogPostSchema.virtual('authorName').get(function() {
+    return `${this.author.firstName} ${this.author.lastName}`.trim();
+  });
+  
+  blogPostSchema.methods.serialize = function() {
+    return {
+      id: this._id,
+      author: this.authorName,
+      content: this.content,
+      title: this.title,
+      created: this.created
+    };
+  };
+  
+  const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+  
+  module.exports = {BlogPost};
